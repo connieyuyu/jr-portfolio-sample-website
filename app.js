129cba85eeb414c9e91b9d0cc29d267608075017
connie.yu <imconnieyu@gmail.com>
2020-12-22 10:27:05 +1000

    Modified layout and added javascript

---
+++ app.js
@@ -0,0 +1,25 @@
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-link');
+  const navLinks = document.querySelectorAll('.nav-link li');
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.3s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+  });
+};
+
+const app = () => {
+  navSlide();
+};
+
+app();
